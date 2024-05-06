894fd57113951bb5a1b9967224ef2df8881029e9
Vladimir Galajda <vladimir@galajda.com>
2024-05-06 20:28:31 +0200

    Add folding setup

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -3,3 +3,8 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 
 vim.opt.whichwrap:append '<>[]'
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
